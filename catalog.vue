5b9df217d68f9bd3855f784c07d784bd38a258c1
yungx666 <yungx21sxx@gmail.com>
2022-05-06 13:24:45 +0300

    mobile v

---
+++ catalog.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "catalog"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/sidebarMobile.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "sidebarMobile"
+}
+</script>
+
+<style scoped>
+
+</style>
